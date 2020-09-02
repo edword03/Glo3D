eac5d6787d2868cb77c3311c01a1eea3fe6c04d9
edword03 <veretennikov_dmitriy01@mail.ru>
2020-09-02 11:57:38 +0400

    feat: added timer on the site

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+  
+  //timer
+  function countTimer(deadline){
+    const timerHours = document.querySelector('#timer-hours'),
+          timerMinutes = document.querySelector('#timer-minutes'),
+          timerSeconds = document.querySelector('#timer-seconds');
+
+    function getTimeRemaind() {
+      let  dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRenaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRenaining % 60),
+            minutes = Math.floor((timeRenaining / 60) % 60),
+            hours = Math.floor((timeRenaining / 60 / 60));
+            return {timeRenaining, seconds, minutes, hours};
+    }
+    
+    const addZero = n => n < 10 ? '0' + n : n;
+
+    function updateClock() {
+      let timer = getTimeRemaind();
+
+      function changeValueTimer() {
+        timerHours.textContent = `${addZero(timer.hours)}`;
+        timerMinutes.textContent = `${addZero(timer.minutes)}`;
+        timerSeconds.textContent = `${addZero(timer.seconds)}`;
+      }
+
+      if (timer.timeRenaining > 0) {
+        changeValueTimer();
+      } else {
+        timerHours.textContent = addZero(0);
+        timerMinutes.textContent = addZero(0);
+        timerSeconds.textContent = addZero(0);
+      }
+
+    }
+    setInterval(updateClock, 1000);
+  }
+
+  countTimer('23 september 2020');
+});
