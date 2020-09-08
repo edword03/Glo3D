952209dd0e3a3fa9008fe684687007a0971e9777
edword03 <veretennikov_dmitriy01@mail.ru>
2020-09-08 22:25:21 +0400

    feat: added calculator

--- scripts/main.js
+++ scripts/main.js
@@ -238,6 +238,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   slider();
 
+  //changing img
   const getDataImg = () => {
     const command = document.querySelector('.command');
     command.addEventListener('mouseover', (event) => {
@@ -255,6 +256,7 @@ window.addEventListener('DOMContentLoaded', () => {
   };
   getDataImg();
 
+  //validation of inputs
   const calcCost = () => {
     const calcBlock = document.querySelector('.calc-block');
     calcBlock.addEventListener('input', (event) => {
@@ -266,4 +268,48 @@ window.addEventListener('DOMContentLoaded', () => {
   }; 
   
   calcCost();
+
+  //calculator
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+      countValue = 1,
+      dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+       squareValue = +calcSquare.value;
+
+       if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+       }
+
+       if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+       } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+       }
+
+       if (typeValue && squareValue) {
+         total = price * typeValue * squareValue * countValue * dayValue;
+       }
+
+      totalValue.textContent = total;
+    };
+
+      calcBlock.addEventListener('change', (event) => {
+        const target = event.target;
+
+        if (target.matches('select') || target.matches('input')) {
+            countSum();
+        }
+      });
+
+  };
+  calc(100);
 });
