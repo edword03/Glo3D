86a12fe8bbed63ae202d436174220219455c0e49
edword03 <veretennikov_dmitriy01@mail.ru>
2020-09-09 19:16:20 +0400

    fix: fixs from branch lesson23

--- scripts/main.js
+++ scripts/main.js
@@ -241,16 +241,21 @@ window.addEventListener('DOMContentLoaded', () => {
   //changing img
   const getDataImg = () => {
     const command = document.querySelector('.command');
+    let img;
+    
     command.addEventListener('mouseover', (event) => {
       let target = event.target;
       if (target.matches('.command__photo')) {
-        let img = target.src;
+        img = target.src;
         target.src = target.dataset.img;
-        command.addEventListener('mouseout', () => {
-          if (target.matches('.command__photo')) {
-            target.src = img;
-          }
-        });
+      }
+      
+    });
+
+    command.addEventListener('mouseout', (event) => {
+      let target = event.target;
+      if (target.matches('.command__photo')) {
+        target.src = img;
       }
     });
   };
@@ -261,7 +266,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const calcBlock = document.querySelector('.calc-block');
     calcBlock.addEventListener('input', (event) => {
       let target = event.target;
-      if (target.matches('.calc-item')) {
+      if (target.matches('input')) {
         target.value = target.value.replace(/[^\d]/g, '');
       }
     });
