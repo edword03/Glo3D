9dc7c6622df3421945cb348c7ecd6c8e95c51d36
edword03 <veretennikov_dmitriy01@mail.ru>
2020-09-20 14:02:27 +0400

    fix: fixed timer

--- src/modules/countTimer.js
+++ src/modules/countTimer.js
@@ -3,6 +3,8 @@ const countTimer = (deadline) => {
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
 
+  let intervalID;
+
   const getTimeRemaind = () => {
     const  dateStop = new Date(deadline).getTime(),
           dateNow = new Date().getTime(),
@@ -18,19 +20,20 @@ const countTimer = (deadline) => {
   const updateClock = () => {
     const timer = getTimeRemaind();
     
-      timerHours.textContent = `${addZero(timer.hours)}`;
-      timerMinutes.textContent = `${addZero(timer.minutes)}`;
-      timerSeconds.textContent = `${addZero(timer.seconds)}`;
+    timerHours.textContent = `${addZero(timer.hours)}`;
+    timerMinutes.textContent = `${addZero(timer.minutes)}`;
+    timerSeconds.textContent = `${addZero(timer.seconds)}`;
     
-    if (timer.timeRenaining > 0) {
-      setInterval(updateClock, 1000);
-    } else {
+    if (timer.timeRenaining <= 0) {
       timerHours.textContent = addZero(0);
       timerMinutes.textContent = addZero(0);
       timerSeconds.textContent = addZero(0);
-    }
+      clearInterval(intervalID);
+    } 
   };
+  
   updateClock();
+  intervalID = setInterval(updateClock, 1000);
 };
 
 export default countTimer;
